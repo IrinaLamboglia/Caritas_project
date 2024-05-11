d59ece8b3f2dcb72d57e593a5241d7506cc2c9e6
Irina <irinalamboglia@gmail.com>
2024-05-11 20:05:48 -0300

    Mejoras

--- login/agregarCategoria/views.py
+++ login/agregarCategoria/views.py
@@ -4,12 +4,13 @@ from core.models import Categoria
 
 def agregar_categoria(request):
     error_message = None
+    exito_message = "Categoria agreda exitosamente"
     if request.method == 'POST':
         nombre_categoria = request.POST.get('nombre_categoria')
         if not Categoria.objects.filter(nombre=nombre_categoria).exists():
             categoria = Categoria.objects.create(nombre=nombre_categoria)
             # Actualizar la lista de productos (a implementar)
-            return redirect('home')  # Redirigir a la página de inicio o a donde sea necesario
+            return  render(request, 'agregar_categoria/aceptarCategoria.html', {'exito_message': exito_message})  # Redirigir a la página de inicio o a donde sea necesario
         else:
             error_message = "La categoría ya está registrada"
     return render(request, 'agregar_categoria/aceptarCategoria.html', {'error_message': error_message})
--- login/log_out/views.py
+++ login/log_out/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 def exit(request):
     if request.method == 'POST':
         logout(request)
+        print()
         return redirect('login')
-    return render(request, 'logOut/confirmar_salida.html')
+   # return render(request, 'core/base.html')
 
