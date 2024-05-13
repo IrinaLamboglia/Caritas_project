d49f179d9c6ceb73c4fb5e124b49c6379bc0bbe4
Martina <martinacolombo@me.com>
2024-05-13 18:16:58 -0300

    edicion_ayudante

--- login/administrador/views.py
+++ login/administrador/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from core.models import Administrador
+from django.contrib import messages
 
 def alta(request):
     if request.method == 'POST':
@@ -24,23 +25,28 @@ def alta(request):
                 mensaje_error="No se puede registrar, ya esta registrado como usuario"
                 return render(request, 'admin/alta.html', {'mensaje_error': mensaje_error})
             else:
-                # Si no existe, crear un nuevo ayudante
-                nuevo_ayudante=Administrador.objects.create(
-                    nombre_usuario=nombre_usuario,
-                    nombre=nombre,
-                    apellido=apellido,
-                    fecha_nacimiento=fecha_nacimiento,
-                    telefono=telefono,
-                    contraseña=contraseña,
-                    dni=dni,
-                    filial=filial
-                )
-                # Enviar el correo al ayudante
-                enviar_correo_ayudante(nuevo_ayudante)
-                # Mostrar el mensaje de éxito
-                mensaje_exito= "Se mando el email al ayudante con sus datos y se registraron los datos exitosamente."
-                return render(request,'admin/alta.html',{'mensaje_exito':mensaje_exito})
-    # Si la solicitud no es POST, simplemente renderiza el formulario
+                # Verificar la longitud de la contraseña
+                if len(contraseña) < 6:
+                    mensaje_error = "La contraseña debe tener al menos 6 caracteres."
+                    return render(request, 'admin/alta.html', {'mensaje_error': mensaje_error})
+                else:
+                    # Si no existe, crear un nuevo ayudante
+                    nuevo_ayudante=Administrador.objects.create(
+                        nombre_usuario=nombre_usuario,
+                        nombre=nombre,
+                        apellido=apellido,
+                        fecha_nacimiento=fecha_nacimiento,
+                        telefono=telefono,
+                        contraseña=contraseña,
+                        dni=dni,
+                        filial=filial
+                    )
+                    # Enviar el correo al ayudante
+                    enviar_correo_ayudante(nuevo_ayudante)
+                    # Mostrar el mensaje de éxito
+                    mensaje_exito= "Se mando el email al ayudante con sus datos y se registraron los datos exitosamente."
+                    return render(request,'admin/alta.html',{'mensaje_exito':mensaje_exito})
+        # Si la solicitud no es POST, simplemente renderiza el formulario
     return render(request, 'admin/alta.html')
 
 def enviar_correo_ayudante(ayudante):
@@ -48,4 +54,3 @@ def enviar_correo_ayudante(ayudante):
     mensaje = f"¡Hola {ayudante.nombre} {ayudante.apellido}! Te hemos registrado en nuestra plataforma como ayudante. Te enviamos tu informacion para que puedas iniciar sesion: Nombre de usuario: {ayudante.nombre_usuario} y la contraseña:{ayudante.contraseña}. Saludos."
     correo_destino = ayudante.nombre_usuario
     send_mail(asunto, mensaje, 'tucorreo@gmail.com', [correo_destino])
-
