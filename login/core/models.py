2cc1944be7995f931cd7f4e01c4bfa8532b5f35b
gggc1 <giulianaacestorame@gmail.com>
2024-06-05 14:29:25 -0300

    cambios

--- login/core/models.py
+++ login/core/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
-
 
 # Create your models here.
 
 
 
+#uso la bd de django para almacenar los intentos fallidos para el inicio de sesion
 class FailedLoginAttempt(models.Model):
     email = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
 
-
+#para registro
+#  ingresa nombre de usuario “martinacolombo@gmail.com”,
+# contraseña “Marti2”, dni “44851840”, 
+# fecha de nacimiento:06/05/2003, 
+# teléfono “222884345”,
+# Nombre y
+# Apellido: Martina colombo y presiona “aceptar”
 
 class Usuario(AbstractUser):
     email = models.EmailField(unique=True)
@@ -26,7 +31,7 @@ class Usuario(AbstractUser):
     contraseña = models.CharField(max_length=128, validators=[MinLengthValidator(6)], default='valor_predeterminado')  # Campo para almacenar la contraseña cifrada
     last_login = models.DateTimeField(verbose_name='last login', blank=True, null=True)
     tipo= models.CharField(max_length=30, default="")
-    puntuacion = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 
 
@@ -35,39 +40,4 @@ class UsuarioBloqueado(models.Model):
 
 
 class porDesbloquear(models.Model):
-    email= models.EmailField(unique=True)  
-    
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
-    estado = models.BooleanField(default=True)
-    def __str__(self):
-        return self.nombre
-
-
-
-class Publicacion(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.CharField(max_length=400)
-    nuevo = models.BooleanField(default=True)
-    estado = models.BooleanField(default=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null = True)
-    imagen = models.ImageField(upload_to='media/publicaciones/', null=True, blank=True)
-
-    def __str__(self):
-        return self.titulo
-    
-email= models.EmailField(unique=True)
-
-
-
-class Solicitud(models.Model):
-    solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, related_name='solicitudes')
-    fecha_solicitud = models.DateTimeField(default=timezone.now)
-    estado = models.BooleanField(default=False)
-    publicacionOfrecida = models.ForeignKey(Publicacion, on_delete=models.CASCADE, related_name='ofrecimientos')
-    realizado= models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.solicitante} solicita {self.publicacion}"
+    email= models.EmailField(unique=True)
--- login/login/settings.py
+++ login/login/settings.py
@@ -160,6 +160,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-#Carpeta media para pillow
-MEDIA_URL = '/media/'
-MEIDA_ROOT = os.path.join(BASE_DIR,'media')
